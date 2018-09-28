cf55fd4b82bcbac1f0bee32d13cee91e764ba57e
Awet Fessha <awetkifle94@gmail.com>
2018-09-28 14:07:52 +0100

    error handling
    Signed-off-by: Awet Fessha <awetkifle94@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,40 @@ public class Main {
     private static Exam exam = new Exam();
 
     public static void main(String[] args) {
-        boolean menuLoop = true;
-        while (menuLoop) {
-            Exam.displayMenu();
-            int choice = sc.nextInt();
+
+            boolean menuLoop = true;
+            while (menuLoop) {
+
+                try {
+                    Exam.displayMenu();
+                    int choice = Integer.parseInt(sc.nextLine());
 
 
-            switch (choice) {
-                case 1:
-                    exam.addStudents();
-                    break;
-                case 2:  exam.getgrade();
-                    break;
-                case 3:
-                    exam.displayStudentInfo();
-                    break;
-                case 4:
-                    exam.displayStudents();
-                    break;
-                case 5:
-                    System.exit(0);
+                switch (choice) {
+                    case 1:
+                        exam.addStudents();
+                        break;
+                    case 2:
+                        exam.getgrade();
+                        break;
+                    case 3:
+                        exam.displayStudentInfo();
+                        break;
+                    case 4:
+                        exam.displayStudents();
+                        break;
+                    case 5:
+                        System.exit(0);
 
+                }
             }
+                catch(NumberFormatException e){
+                    System.out.println("The value you entered is invalid, please enter a value between 1 and 5");
+                }
+                catch(InputMismatchException e){
+                    System.out.println("The value you entered doesn't match, please enter a value between 1 and 5");
+                }
         }
+
     }
 }
